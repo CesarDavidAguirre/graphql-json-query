c37bf364c823bf6e6de8b9e93b58121a10abdbb0
Cesar Aguirre <cesar.aguirre@underdog.mx>
2021-11-12 22:25:43 -0300

    delete dist to repo

--- dist/builders/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildReturnData = exports.biuldJsonToData = void 0;
-var jsonToData_1 = require("./jsonToData");
-Object.defineProperty(exports, "biuldJsonToData", { enumerable: true, get: function () { return jsonToData_1.biuldJsonToData; } });
-var returnData_1 = require("./returnData");
-Object.defineProperty(exports, "buildReturnData", { enumerable: true, get: function () { return returnData_1.buildReturnData; } });
-//# sourceMappingURL=index.js.map
